5f42939cab0de8aab5e54d88d9b04e99f84c3109
Harsh Indoria <68156841+Indoriajs@users.noreply.github.com>
2021-08-18 20:21:52 +0530

    Add files via upload

---
+++ first-pageJS.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("cv-button").addEventListener("click", function(){
+    window.open("../html/cv_page.html", "_self");
+});
+
+document.getElementById("resume-button").addEventListener("click", function(){
+    window.open("../html/resume_page.html", "_self");
+});
+
+document.getElementById("project-button").addEventListener("click", function(){
+    window.open("../html/project_page.html", "_self");
+});
---
+++ resume-pageJS.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById("resume-downloadBtn").addEventListener("click", function(){
+    window.open("../res/pdf/Resume.pdf");
+});
